4eebef15c7ee01b757dd053090575eae4317cabf
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-13 12:10:20 +0000

    Recaching data as of Sat Sep 13 12:10:20 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -563,7 +563,7 @@ initData({},
   {
     "city": "Bucharest", 
     "name": "People's Climate March", 
-    "rsvp_count": 47, 
+    "rsvp_count": 48, 
     "country": "Romania", 
     "start_time": "13:00:00", 
     "venue": "TBC", 
