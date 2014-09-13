78ce7b7a4fc94335426c8f05896fd8ab838cdfea
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-13 20:12:08 +0000

    Recaching data as of Sat Sep 13 20:12:08 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -19,7 +19,7 @@ initData({},
   {
     "city": "Rio de Janeiro", 
     "name": "Caminhada pelo Clima - RIO", 
-    "rsvp_count": 1162, 
+    "rsvp_count": 1163, 
     "country": "Brazil", 
     "start_time": "10:30:00", 
     "venue": "Praia de Ipanema -- Posto 8, das 10h30 \u00e0s 14h30", 
