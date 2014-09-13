06ed6b1342b314b949f6a6c2916c7addface52a7
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-13 10:02:07 +0000

    Recaching data as of Sat Sep 13 10:02:07 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -2883,7 +2883,7 @@ initData({},
   {
     "city": "Rennes", 
     "name": "Marche Pour le Climat", 
-    "rsvp_count": 81, 
+    "rsvp_count": 82, 
     "country": "France", 
     "start_time": "13:00:00", 
     "venue": "M\u00e9tro R\u00e9publique", 
