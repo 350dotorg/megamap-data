2fc2d7fa70ccb209f770ee4ac2b4ebb79ee57073
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-13 02:46:08 +0000

    Recaching data as of Sat Sep 13 02:46:08 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -115,7 +115,7 @@ initData({},
   {
     "city": "Montreal", 
     "name": "Peoples' Climate March @ Montreal // Marche mondiale pour le climat @ Montr\u00e9al", 
-    "rsvp_count": 218, 
+    "rsvp_count": 219, 
     "country": "Canada", 
     "start_time": "13:00:00", 
     "venue": "Parc Lafontaine", 
