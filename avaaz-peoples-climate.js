82feac9daa88303c610474fb5fa6f26f2e3c8590
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-13 04:37:07 +0000

    Recaching data as of Sat Sep 13 04:37:07 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -16819,7 +16819,7 @@ initData({},
   {
     "city": "Ciudad de M\u00e9xico", 
     "name": "La Marcha Ciudadana contra el Cambio Clim\u00e1tico", 
-    "rsvp_count": 86, 
+    "rsvp_count": 87, 
     "country": "Mexico", 
     "start_time": "13:00:00", 
     "venue": "Zocalo de la Ciudad de M\u00e9xico", 
