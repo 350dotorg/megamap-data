99bd1a44c3e06b2256c4620e7909a5af099d0545
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-14 14:37:08 +0000

    Recaching data as of Sun Sep 14 14:37:08 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -3,7 +3,7 @@ initData({},
   {
     "city": "London", 
     "name": "People's Climate March London", 
-    "rsvp_count": 2240, 
+    "rsvp_count": 2241, 
     "country": "United Kingdom", 
     "start_time": "12:30:00", 
     "venue": "", 
