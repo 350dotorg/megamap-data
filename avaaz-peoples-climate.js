7e99e20464bd0c62e28347cde86ac0b70f0cca5d
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-15 04:48:07 +0000

    Recaching data as of Mon Sep 15 04:48:07 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -13427,7 +13427,7 @@ initData({},
   {
     "city": "Currumbin", 
     "name": "People's Climate March", 
-    "rsvp_count": 52, 
+    "rsvp_count": 53, 
     "country": "Australia", 
     "start_time": "13:00:00", 
     "venue": "Currumbin Beach", 
