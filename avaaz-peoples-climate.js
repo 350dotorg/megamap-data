9a0eceaf84c5390255bd749b9bbb3c4a522630a2
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-15 16:26:10 +0000

    Recaching data as of Mon Sep 15 16:26:10 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -2051,7 +2051,7 @@ initData({},
   {
     "city": "Brasilia", 
     "name": "Uni\u00e3o Avaaz em Bras\u00edlia contra as Mudan\u00e7as Clim\u00e1ticas", 
-    "rsvp_count": 465, 
+    "rsvp_count": 466, 
     "country": "Brazil", 
     "start_time": "10:00:00", 
     "venue": "Esplanada dos Ministerios", 
