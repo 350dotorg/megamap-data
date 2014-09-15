f4dbe2a9608741cd6ed3e97ea85ea9baa485d36e
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-15 02:40:08 +0000

    Recaching data as of Mon Sep 15 02:40:08 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -19747,7 +19747,7 @@ initData({},
   {
     "city": "Goi\u00e2nia", 
     "name": "Caminhada pelo Clima", 
-    "rsvp_count": 174, 
+    "rsvp_count": 175, 
     "country": "Brazil", 
     "start_time": "16:00:00", 
     "venue": "Pra\u00e7a C\u00edvica, setor Sul", 
