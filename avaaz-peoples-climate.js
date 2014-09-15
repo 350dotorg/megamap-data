dcfbb85c20e66e3e3965860f263dc4cb5f7a6961
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-15 04:12:08 +0000

    Recaching data as of Mon Sep 15 04:12:08 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -15715,7 +15715,7 @@ initData({},
   {
     "city": "S\u00e3o Paulo", 
     "name": "Caminhada pelo Clima", 
-    "rsvp_count": 163, 
+    "rsvp_count": 164, 
     "country": "Brazil", 
     "start_time": "13:00:00", 
     "venue": "Pra\u00e7a Campo de Bagatelle - Bairro de Santana - em frente ao Campo de Marte", 
