8d5da0d43c5a7b7ad052aaef8fb0c892d3f61ea7
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-16 22:18:09 +0000

    Recaching data as of Tue Sep 16 22:18:09 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -3875,7 +3875,7 @@ initData({},
   {
     "city": "Boulogne-sur-Mer", 
     "name": "Marche Pour le Climat", 
-    "rsvp_count": 27, 
+    "rsvp_count": 28, 
     "country": "France", 
     "start_time": "13:00:00", 
     "venue": "", 
