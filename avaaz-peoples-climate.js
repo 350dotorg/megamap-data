b684880ab3f899413746e387756d7a42c6f13779
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-16 16:30:15 +0000

    Recaching data as of Tue Sep 16 16:30:15 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -31347,7 +31347,7 @@ initData({},
   {
     "city": "S\u00e3o Carlos", 
     "name": "Caminhada pelo Clima", 
-    "rsvp_count": 14, 
+    "rsvp_count": 15, 
     "country": "Brazil", 
     "start_time": "13:00:00", 
     "venue": "Avenida S\u00e3o Carlos, Pra\u00e7a Coronel Salles", 
