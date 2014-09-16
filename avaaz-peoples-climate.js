ded51ca8b0608e5b8c840aa4ef0c6b48df93452d
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-16 10:47:09 +0000

    Recaching data as of Tue Sep 16 10:47:09 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -4051,7 +4051,7 @@ initData({},
   {
     "city": "Villeneuve-sur-Lot", 
     "name": "Marche Pour le Climat", 
-    "rsvp_count": 18, 
+    "rsvp_count": 19, 
     "country": "France", 
     "start_time": "13:00:00", 
     "venue": "", 
