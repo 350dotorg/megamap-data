ce7fca2069f565f3009d8742fd431b9ce4733d6e
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-16 12:22:09 +0000

    Recaching data as of Tue Sep 16 12:22:09 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -83,7 +83,7 @@ initData({},
   {
     "city": "Greater Sudbury", 
     "name": "People's Climate March", 
-    "rsvp_count": 23, 
+    "rsvp_count": 24, 
     "country": "Canada", 
     "start_time": "13:00:00", 
     "venue": "", 
