1ad9fea352e85faa6bcb4795a94b9112a32d8a91
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-17 00:15:10 +0000

    Recaching data as of Wed Sep 17 00:15:10 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -1347,7 +1347,7 @@ initData({},
   {
     "city": "Toronto", 
     "name": "People's Climate March - Toronto", 
-    "rsvp_count": 484, 
+    "rsvp_count": 485, 
     "country": "Canada", 
     "start_time": "13:00:00", 
     "venue": "", 
