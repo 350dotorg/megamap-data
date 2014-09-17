fdb73666b7002af5fd504d92c63d3acc357aacca
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-17 22:08:08 +0000

    Recaching data as of Wed Sep 17 22:08:08 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -131,7 +131,7 @@ initData({},
   {
     "city": "", 
     "name": "People's Climate March - Auckland", 
-    "rsvp_count": 264, 
+    "rsvp_count": 265, 
     "country": "New Zealand", 
     "start_time": "13:00:00", 
     "venue": "BRITOMART train station", 
