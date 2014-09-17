91242bd179203152cd500bec03f2c67664618c04
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-17 10:01:07 +0000

    Recaching data as of Wed Sep 17 10:01:07 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -19539,7 +19539,7 @@ initData({},
   {
     "city": "Brest", 
     "name": "Marche Pour le Climat", 
-    "rsvp_count": 45, 
+    "rsvp_count": 46, 
     "country": "France", 
     "start_time": "13:00:00", 
     "venue": "brest", 
