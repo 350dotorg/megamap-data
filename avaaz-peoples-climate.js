f4d4fa53dc072527a44a693e606fa4c2513b9313
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-17 18:24:13 +0000

    Recaching data as of Wed Sep 17 18:24:13 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -9603,7 +9603,7 @@ initData({},
   {
     "city": "Islamabad", 
     "name": "People's Climate March", 
-    "rsvp_count": 24, 
+    "rsvp_count": 25, 
     "country": "Pakistan", 
     "start_time": "13:00:00", 
     "venue": "", 
