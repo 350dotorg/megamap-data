5684428ec7651c21224983045b44b89f5c9b1f8a
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-17 06:20:09 +0000

    Recaching data as of Wed Sep 17 06:20:09 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -2867,7 +2867,7 @@ initData({},
   {
     "city": "Lausanne", 
     "name": "Marche Pour le Climat", 
-    "rsvp_count": 158, 
+    "rsvp_count": 159, 
     "country": "Switzerland", 
     "start_time": "13:00:00", 
     "venue": "gare de lausanne", 
