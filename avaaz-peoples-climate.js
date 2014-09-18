0520fbfae8095b18e886887b7d433a97786c8e56
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-18 08:17:09 +0000

    Recaching data as of Thu Sep 18 08:17:09 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -15187,7 +15187,7 @@ initData({},
   {
     "city": "Gen\u00e8ve", 
     "name": "Marche pour le climat- Gen\u00e8ve", 
-    "rsvp_count": 179, 
+    "rsvp_count": 180, 
     "country": "Switzerland", 
     "start_time": "14:00:00", 
     "venue": "Gen\u00e8ve, Plaine de Plainpalais, \u00e0 c\u00f4t\u00e9 du skate-park", 
