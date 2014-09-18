09858f90e4114ccdc90474d606223c42d50f8b44
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-18 12:18:10 +0000

    Recaching data as of Thu Sep 18 12:18:10 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -27747,7 +27747,7 @@ initData({},
   {
     "city": "Saint-Pierre-de-Bressieux", 
     "name": "Marche Pour le Climat", 
-    "rsvp_count": 2, 
+    "rsvp_count": 3, 
     "country": "France", 
     "start_time": "13:00:00", 
     "venue": "Gymnase de st pierre de bressieux", 
