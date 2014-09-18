67e56a5876cc1d837f4b4ab762f312420ea711e2
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-18 14:44:10 +0000

    Recaching data as of Thu Sep 18 14:44:10 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -3043,7 +3043,7 @@ initData({},
   {
     "city": "Porto Alegre", 
     "name": "Caminhada pelo Clima", 
-    "rsvp_count": 546, 
+    "rsvp_count": 547, 
     "country": "Brazil", 
     "start_time": "10:30:00", 
     "venue": "Parque Farroupilha - Reden\u00e7\u00e3o", 
