5d9dd40437445ea920bb9bc739e24730bbaa408e
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-18 02:50:09 +0000

    Recaching data as of Thu Sep 18 02:50:09 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -1283,7 +1283,7 @@ initData({},
   {
     "city": "Bogota", 
     "name": "Bogot\u00e1 Marcha X el Clima", 
-    "rsvp_count": 460, 
+    "rsvp_count": 461, 
     "country": "Colombia", 
     "start_time": "10:00:00", 
     "venue": "Parque Nacional hasta Plaza de los Periodistas", 
