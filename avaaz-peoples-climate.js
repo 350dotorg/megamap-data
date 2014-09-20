64aa5d5015acf526c699d223da763b51c5426a0e
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-09-20 18:38:08 +0000

    Recaching data as of Sat Sep 20 18:38:08 UTC 2014 (committed by a robot)

--- avaaz-peoples-climate.js
+++ avaaz-peoples-climate.js
@@ -17251,7 +17251,7 @@ initData({},
   {
     "city": "Cherbourg-Octeville", 
     "name": "Marche Pour le Climat", 
-    "rsvp_count": 21, 
+    "rsvp_count": 22, 
     "country": "France", 
     "start_time": "13:00:00", 
     "venue": "Mairie de Cherbourg", 
