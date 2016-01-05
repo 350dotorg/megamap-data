cb984fcdd007abfcf2ac0998d4de61c7d092cc1a
Ethan Jucovy <ethan.jucovy@gmail.com>
2016-01-05 00:00:04 +0000

    Recaching data as of Tue Jan  5 00:00:04 UTC 2016 (committed by a robot)

--- actionnetwork-events/350-brooklyn.js
+++ actionnetwork-events/350-brooklyn.js
@@ -1,1 +1,1 @@
-ANEventsCallback([]);
+ANEventsCallback([{"url": "https://actionnetwork.org/events/bi-weekly-general-meeting-4", "start": "2016-01-05T19:00:00Z", "description": "<p>Please join us for our first bi-monthly general meeting of the year!&nbsp;Tuesday, January 5th at 7pm. St. Lydia\u2019s Church, 304 Bond Street, Brooklyn. All are welcome!</p>", "title": "Bi-weekly general meeting"}]);
