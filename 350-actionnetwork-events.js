2a8b5f9939046d1dde814af6dbf6d0ea045446be
Ethan Jucovy <ethan.jucovy@gmail.com>
2016-03-14 06:53:11 -0400

    Update 350-actionnetwork-events.js

--- 350-actionnetwork-events.js
+++ 350-actionnetwork-events.js
@@ -1,3 +1,4 @@
+var emptyMsg;
 window.ANEventsCallback = function(resp) {
     var ul = jQuery("<ul>");
     jQuery.each(resp, function(i, o) {
@@ -6,10 +7,14 @@ window.ANEventsCallback = function(resp) {
       li.html("<b>"+start+"</b>: <a href='"+o.url+"'>"+o.title+"</a>")
         .appendTo(ul);
     });
+    if (emptyMsg && resp.length === 0) {
+        $("<li>").text(emptyMsg).appendTo(ul);
+    }
     ul.appendTo("#event-list");
   };
 jQuery(document).ready(function() {
   var group = jQuery("script[src$='350-actionnetwork-events.js']").data("group");
+  emptyMsg = jQuery("script[src$='350-actionnetwork-events.js']").data("empty-message");
   jQuery.getScript('https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js', function() {
     jQuery("<script async type='text/javascript' src='//350dotorg.github.io/megamap-data/actionnetwork-events/"+group+".js'>").appendTo(document.body);
   });
